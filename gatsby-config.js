458b1c54fe3458d15f8cdd36e1d26c73e2639b95
Dhony <dhony.web@gmail.com>
2020-11-22 11:29:34 -0300

    Alteration on Bio Component and siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `Dhony Silva`,
+    author: `dhonysilva`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://dhony-blog.netlify.app/blog/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `dhonysilva`,
     },
   },
   plugins: [
